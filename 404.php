b9e6c169d2878e2d7ad78a4f64db98090b51ea85
Josh Armentano <josh@abidewebdesign.com>
2019-03-14 12:05:50 -0700

    Added 404 page.

--- 404.php
+++ 404.php
@@ -10,6 +10,18 @@
 get_header();
 ?>
 
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div id="global-store-container">
+		<div class="container text-center py-4 text-center">
+								
+			<h1 class="text-primary mb-2">Page Not Found</h1>
+			<p class="lead">The page you are looking for does not exist.</p>
+			<a href="<?php echo home_url(); ?>" class="btn btn-primary"><span>Return Home</span></a>
+
+		</div>
+	</div>
+</div>
+
 
 <?php
 get_footer();
