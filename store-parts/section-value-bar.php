e516e7e2890f348933c443f0ee94e8fe83cec4b1
Josh Armentano <josh@abidewebdesign.com>
2019-04-15 16:02:31 -0700

    Additional conditional statements.

--- store-parts/section-value-bar.php
+++ store-parts/section-value-bar.php
@@ -1,17 +1,19 @@
-<div id="section-value-bar" class="sb box">
-	<div class="container">
-		<div class="row justify-content-center text-center">
-			<div class="col">
-				<ul class="list-inline mb-0">
-					
-					<?php while (have_rows('value_list', $store)): the_row() ?>
-					
-						<li class="list-inline-item"><i class="fas fa-check-circle"></i> <?php the_sub_field('value_list_item'); ?></li>
-					
-					<?php endwhile; ?>
-					
-				</ul>
+<?php if (have_rows('value_list', $store)): ?>
+	<div id="section-value-bar" class="sb box">
+		<div class="container">
+			<div class="row justify-content-center text-center">
+				<div class="col">
+					<ul class="list-inline mb-0">
+						
+						<?php while (have_rows('value_list', $store)): the_row() ?>
+						
+							<li class="list-inline-item"><i class="fas fa-check-circle"></i> <?php the_sub_field('value_list_item'); ?></li>
+						
+						<?php endwhile; ?>
+						
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+<?php endif; ?>
