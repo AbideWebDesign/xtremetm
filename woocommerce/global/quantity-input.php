1eeb492a0c07faf337b4adec8887d06b06febc4e
Josh Armentano <josh@abidewebdesign.com>
2019-04-16 13:32:00 -0700

    Updates to quantity override.

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -2,36 +2,54 @@
 /**
  * Product quantity inputs
  *
- * @author  WooThemes
+ * This template can be overridden by copying it to yourtheme/woocommerce/global/quantity-input.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 2.1.0
+ * @version 3.4.0
  */
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-global $product;
-$defaults = array(
-	'max_value'   => apply_filters( 'woocommerce_quantity_input_max', '', $product ),
-	'min_value'   => apply_filters( 'woocommerce_quantity_input_min', '', $product ),
-	'step'        => apply_filters( 'woocommerce_quantity_input_step', '1', $product ),
-);
-if ( ! empty( $defaults['min_value'] ) )
-	$min = $defaults['min_value'];
-else $min = 1;
-if ( ! empty( $defaults['max_value'] ) )
-	$max = $defaults['max_value'];
-else $max = 20;
-if ( ! empty( $defaults['step'] ) )
-	$step = $defaults['step'];
-else $step = 1;
-?>
-<div class="quantity_select">
-	<select name="<?php echo esc_attr( $input_name ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="qty">
-	<?php
-	for ( $count = $min; $count <= $max; $count = $count+$step ) {
-		if ( $count == $input_value )
-			$selected = ' selected';
-		else $selected = '';
-		echo '<option value="' . $count . '"' . $selected . '>' . $count . '</option>';
-	}
+
+defined( 'ABSPATH' ) || exit;
+
+if ( $max_value && $min_value === $max_value ) {
 	?>
-	</select>
-</div>
+	<div class="quantity hidden">
+		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>" />
+	</div>
+	<?php
+} else {
+	
+	if ( ! empty( $min_value ) )
+		$min = $min_value;
+	else $min = 1;
+	if ( ! empty( $max_value ) )
+		$max = $max_value;
+	else $max = 20;
+	
+	$step = 1;
+	
+	/* translators: %s: Quantity. */
+	$labelledby = ! empty( $args['product_name'] ) ? sprintf( __( '%s quantity', 'woocommerce' ), strip_tags( $args['product_name'] ) ) : '';
+	?>
+	<div class="quantity-select">
+		<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></label>
+		<select name="<?php echo esc_attr( $input_name ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="qty">
+		<?php
+		for ( $count = $min; $count <= $max; $count = $count+$step ) {
+			if ( $count == $input_value )
+				$selected = ' selected';
+			else $selected = '';
+			echo '<option value="' . $count . '"' . $selected . '>' . $count . '</option>';
+		}
+		?>
+		</select>
+
+	</div>
+	<?php
+}
