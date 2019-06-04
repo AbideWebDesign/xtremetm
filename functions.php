5d684bdf21df9e3109cb92dbe03a4a6cec8b34f6
Josh Armentano <josh@abidewebdesign.com>
2019-06-04 08:50:52 -0700

    Fixed bug with ACH discount function.

--- functions.php
+++ functions.php
@@ -939,7 +939,7 @@ function woocommerce_add_ach_discount( WC_Cart $cart ){
 			$terms = wp_get_post_terms($cart_item['product_id'], 'product_cat', false);
 			$store = get_product_store($terms);
 
-			if ( $store->name == 'Rehz' || $store->name == 'XtremeTM' ) {
+			if ( $store->name == 'Rehz' || $store->name == 'Primis' ) {
 				
 				return;
 				
