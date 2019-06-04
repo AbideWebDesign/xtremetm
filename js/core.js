1f9f4a392486b837ac4c2056eeca8f0b750f2bc1
Josh Armentano <josh@abidewebdesign.com>
2019-06-04 09:06:50 -0700

    Added hiding shipping address fields when shipping to events.

--- js/core.js
+++ js/core.js
@@ -1,28 +1,49 @@
-(function($) {
-	$(document).ready(function(){
-		$('#search').on("click",(function(e){
+( function($) {
+	
+	$( document ).ready(function(){
+		
+		$( '#search' ).on("click",( function(e){
+			
 			$(".form-group").addClass("sb-search-open");
 			$("#search-text").show();
-			e.stopPropagation();
-		}));
+			e.stopPropagation();			
 			
-		$(document).on("click", function(e) {
-			if ($(e.target).is("#search") === false) {
+		} ) );
+			
+		$( document ).on("click", function(e) {
+			
+			if ( $(e.target).is("#search") === false ) {
+			
 				$(".form-group").removeClass("sb-search-open");
+			
 			}
-		});
+	
+		} );
 				
-		$("#ship-to-event-checkbox").click(function (e) {
-	        if ($(this).is(':checked')) {
+		$( "#ship-to-event-checkbox" ).click( function(e) {
+	        
+	        if ( $(this).is(':checked') ) {
+		    
 		        $('#ship-to-event').slideDown('fast');
+		        $('.shipping_address .address-field').hide();
+		        $('.shipping_address #shipping_company_field').hide();
+	        
 	        } else {
+		    
 		        $('#ship-to-event').slideUp('fast');
+		        $('.shipping_address .address-field').show();
+		        $('.shipping_address #shipping_company_field').show();
+	        
 	        }
-    	});
-    });
+    	
+    	} );
     
-    $( document.body ).on( 'change', 'input[name="payment_method"]', function () {
+    } );
+    
+    $( document.body ).on( 'change', 'input[name="payment_method"]', function() {
+	    
 		$( 'body' ).trigger( 'update_checkout' );
-	});
+	
+	} );
 		
-})(jQuery);
+} )( jQuery );
