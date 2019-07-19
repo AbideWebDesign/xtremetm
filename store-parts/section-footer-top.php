b80061afa7c6300bb077a2553f9d8695e2f311f5
Josh Armentano <josh@abidewebdesign.com>
2019-07-19 16:34:31 -0700

    Added promotional page template.

--- store-parts/section-footer-top.php
+++ store-parts/section-footer-top.php
@@ -4,7 +4,7 @@
 			<div class="row align-items-center">
 				<div class="col-lg-3 text-center text-lg-left mb-2 mb-lg-0">
 					<a href="<?php the_field('footer_top_button_link', $store); ?>" <?php echo (get_field('footer_top_link_type', $store) ? 'target="_blank"' : ''); ?>>
-						<?php echo wp_get_attachment_image(get_field('footer_top_logo', $store), false, array('class'=>'img-fluid')); ?>
+						<?php echo wp_get_attachment_image(get_field('footer_top_logo', $store), 'col-3', false, array('class'=>'img-fluid')); ?>
 					</a>
 				</div>
 				<div class="col-lg-7 mb-2 mb-lg-0">
