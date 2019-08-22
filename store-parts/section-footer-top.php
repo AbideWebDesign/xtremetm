ca4fa6d7d6b7537d2547c15280d9d84c1bf8d48f
Josh Armentano <josh@abidewebdesign.com>
2019-08-22 07:46:15 -0700

    Responsive style updates.

--- store-parts/section-footer-top.php
+++ store-parts/section-footer-top.php
@@ -7,10 +7,10 @@
 						<?php echo wp_get_attachment_image(get_field('footer_top_logo', $store), 'col-3', false, array('class'=>'img-fluid')); ?>
 					</a>
 				</div>
-				<div class="col-lg-7 mb-2 mb-lg-0">
+				<div class="col-lg-6 mb-2 mb-lg-0">
 					<p class="mb-0"><?php the_field('footer_top_description', $store); ?></p>
 				</div>
-				<div class="col-lg-2 text-center text-lg-left">
+				<div class="col-lg-3 text-center text-lg-left">
 					<a href="<?php the_field('footer_top_button_link', $store); ?>" <?php echo (get_field('footer_top_link_type', $store) ? 'target="_blank"' : ''); ?> class="btn btn-primary btn-white"><span>Learn More <i class="fas fa-chevron-right"></i></span></a>
 				</div>
 			</div>
