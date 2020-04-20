2c7d8d40a0b0f5f3ed49e7264d601fa709096d65
Josh Armentano <josh@abidewebdesign.com>
2020-04-20 14:13:34 -0700

    Updated clear inventory function to fire on button click.

--- page-clear-inventory.php
+++ page-clear-inventory.php
@@ -7,6 +7,30 @@
 
 wp_head();
 
-clear_inventory();
+?>
+
+<?php if ( $_POST ): ?>
+	
+	<?php clear_inventory(); ?>
+
+<?php else: ?>
+
+	<div class="d-flex h-100 align-items-center">
+		
+		<div class="text-center w-100">
+			
+			<form name="clear-inventory" id="clear-inventory" method="post" action="<?php echo  get_permalink($page_id); ?>">
+				
+				 <input class="btn btn-primary" type="submit" name="submit" value="Clear Inventory" id="submit"/>
+				
+			</form>
+		
+		</div>
+		
+	</div>
+	
+<?php endif; ?>
+
+<?php
 
 wp_footer();
